0db7537c53be6dd59e5b5bc90619185fd90c4a4e
German Villalobos <german.villalobos0@gmail.com>
2014-11-03 11:43:07 -0600

    Revert "ScholarshipClass"

--- OOP/src/ScholarshipSystem/Admin.java
+++ OOP/src/ScholarshipSystem/Admin.java
@@ -1,58 +1,7 @@
 package ScholarshipSystem;
 
-import java.util.*;
-
 /**
  * Created by alice on 10/22/14.
  */
 public class Admin {
-
-	private int adminID;
-	private String password;
-
-	// java provides default constructor that does exactly this;
-	// good practice to be explicit with it?
-	public Admin()
-	{
-		adminID = -1;
-		password = "";
-	}
-	public Admin(int id, String pw)
-	{
-		adminID = id;
-		password = pw;
-	}
-
-	public void setAdminID(int id) { adminID = id; }
-	public void setPassword(String pw) { password = pw; } // how does security work we just don't know
-
-	public int getAdminID() { return adminID; }
-	public String getPassword() { return password; } // ditto
-
-	public void addScholarship()
-	{
-		// I/O skeleton just to show what info will be needed from admin.
-		
-		// TODO: Create new Scholarship object with provided data: need Scholarship constructor(s) to be written.
-		
-		// TODO: Create Criteria object for scholarship requirements -
-		// pass GPA, major, hours to Scholarship for it to pass to
-		// Criteria? Need Criteria constructor(s) to be written.
-
-		Scanner stdin = new Scanner(System.in);
-
-		System.out.println("Enter the scholarship name: ");
-		String name = stdin.nextLine();
-
-		System.out.println("Enter the scholarship amount: ");
-		double amount = stdin.nextDouble();
-
-		System.out.println("Enter the maximum number of students: ");
-		int studentLimit = stdin.nextInt();
-
-		System.out.println("Enter deadline: ");
-		String deadline = stdin.nextString();
-
-	}
-
 }
--- OOP/src/ScholarshipSystem/Student.java
+++ OOP/src/ScholarshipSystem/Student.java
@@ -4,24 +4,4 @@ package ScholarshipSystem;
  * Created by alice on 10/22/14.
  */
 public class Student {
-
-	private int studentID;
-	private String password;
-	private Criteria studentCrit;
-
-	public Student()
-	{
-		studentID = -1;
-		password = "";
-		studentCrit = null; // does this do in java what I want it to do as if this is C++?
-	}
-	public Student(int id, String pw, double gpa, String major, int hours)
-	{
-		studentID = id;
-		password = pw;
-		// studentCrit = new Criteria( // pass gpa major & hours )
-		// need Criteria constructor(s) to be written
-		// other option is just to pass Criteria object and assign to studentCrit but I think this is better...data hiding? that might be what this is
-	}
-
 }
